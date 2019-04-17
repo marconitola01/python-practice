024852728a457476b04c455bbfeffc4340720711
marco nitola <33072491+marconitola01@users.noreply.github.com>
2019-04-17 16:33:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,134 +1,116 @@
-#hola mundo en python
-#print("hola mundo");
+import time
+import random
 
-#print("hola");
-#print('¿cual es su nombre socio?');
-
-#miNombre = input();
-
-#print("hola "+miNombre);
-
-
-#adivinar el numero programa de practica
-"""import random;
-print("hola jugador");
-print("como te llamas?");
-
-NombrePlayer = input();
-
-print("bienvenido "+NombrePlayer);
-
-numero = random.randint(1,20);
-
-print("bueno "+NombrePlayer+" estoy pensando en un numero del 1 al 20");
-
-NumeroIntentos = 0;
-
-while NumeroIntentos < 8:
-    print("di tu numero")
-    estimacion = input();
-    estimacion = int(estimacion);
-    if estimacion < numero:
-        print("tu numero esta por debajo");
-        NumeroIntentos += 1;
-
-    if estimacion > numero:
-        print("tu numero esta por encima");
-        NumeroIntentos += 1;
-
-    if estimacion == numero:
-        break;
-
-if estimacion == numero:
-    NumeroIntentos = str(NumeroIntentos);
-    print("¡CORRECTO PAPU! LO LOGRASTE EN " + NumeroIntentos + " INTENTOS");
-
-if estimacion != numero:
-    numero = str(numero);
-    print("fallaste papu estaba pensando en el "+numero);"""""
-
-
-#me dice cuantas letras tiene mi nombre
-"""""""""
-def DecirHola(Nombre):
-    print("hola "+Nombre+" tu nombre tiene "+ str(len(Nombre))+" letras");
-
-Nombre = input();
-DecirHola(Nombre);"""""
-
-#juego marik de los dragons mi pes
-"""""""""
-import random;
-import time;
-
-def mostrarIntro():
-    print("estas en tierra de dragones. Frente a ti")
-    print("hay dos cuevas, en una de ellas el dragon es generoso y")
-    print("compartira su tesoro contigo. E l otro dragon es codicioso y")
-    print("tiene hambre")
-    print("")
-
-def elegirCueva():
-    cueva = '';
-    while cueva !='1' and cueva !='2':
-        print("que cueva eligiras 1 o 2");
-        cueva = input();
-
-    return cueva;
-
-def explorarCueva(cuevaElegida):
-    print("te aproximas a la cueva");
-    time.sleep(2);
-    print("es fria y tenebrosa");
-    time.sleep(2);
-    print("aparece un gran dragon frente a ti abre la jeta y..");
-    time.sleep(2);
-    print();
-
-    cuevaAmigable = random.randint(1,2);
-
-    if cuevaElegida == str(cuevaAmigable):
-        print('te regala su tesoro');
-
+#hallar numero maximo
+"""""""""""
+def max (n1, n2):
+    if n1 < n2:
+        print(n2)
+    elif n2 < n1:
+        print(n1)
     else:
-        print("se lo traga mi pes");
-
-jugarDeNuevo = 'si';
-
-while jugarDeNuevo == 'si' or jugarDeNuevo == 's':
-    mostrarIntro();
-
-    numeroDeCueva = elegirCueva();
-
-    explorarCueva(numeroDeCueva);
-
-    print('¿Quieres jugar de nuevo? si o no')
-
-    jugarDeNuevo = input();
+        print("Son iguales")
+print('introduce un numero 1')
+n1 = int(input())
+print('introduce nuemro 2')
+n2 = int(input())
+print('el mayor es :')
+max(n1,n2)
 """""
 
-#jodiendo un tal
-"""
-import time;
+#tomar tres numeros y devolver el maximo de ellos
+"""""""""
+def maxtr(n1,n2,n3):
+    if n1 > n2 and n1>n3:
+        print(n1)
+    if n2 > n1 and n2 > n3:
+        print(n2)
+    if n3 > n1 and n3 > n2:
+        print(n3)
 
-def suma(x ,y):
-    return x+y;
+print('introduce numero 1')
+numero1 = int(input())
+print('intriduce numero 2')
+numero2 = int(input())
+print('introduce numero 3')
+numero3 = int(input())
+
+print('el numero maximo es: ')
+maxtr(numero1,numero2,numero3)
+
+"""""
 
 
-def sumaAcincuenta():
-    numero = 1;
-    while numero <= 50:
-        print(numero);
-        time.sleep(0.5);
-        numero += 1;
-    if numero == 50:
-        print("acabamo");
+#imprimir el largo de una lista o cadena (se pude gace con len pero alovorgo)
+"""""""""
+def tamaño(Lista):
+    contador = 0
+    for i in Lista:
+        contador += 1
+    return contador
+
+print(tamaño([1,2,3,4]))
+print(tamaño("buenalabanda"))
+"""""
+
+#devolver true si es una vocal y sino false
+"""""""""
+def vocalT(vocal):
+    if vocal == "a" or vocal == "e" or vocal == "i" or vocal=="o" or vocal =="u":
+        return True
+    if vocal == "A" or vocal == "E" or vocal == "I" or vocal == "O" or vocal == "U":
+        return True
+    else:
+        return False
+print('ingrese una vocal')
+vocal = input()
+
+print(vocalT(vocal))
+"""""
+
+#Escribir una funcion sum() y una función multip() que sumen y multipliquen respectivamente todos los números de una lista. Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
+"""""""""
+def sumar(ListaNumeros1):
+    suma = 0
+    for i in ListaNumeros1:
+        suma += i
+    return suma
+
+def multipli(ListaNumeros2):
+    multi = 1
+    for j in ListaNumeros2:
+        multi *= j
+    return multi
+
+print(sumar([1,2,3,4]))
+print(multipli([1,2,3,4]))
+"""""
+
+#Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la cadena "estoy probando" debería devolver la cadena "odnaborp yotse"
+"""""""""
+def palabraInversa(palabra):
+    invertida = ""
+    contador = len(palabra)
+    index = -1
+    while contador >= 1:
+        invertida += palabra[index]
+        index = index +(-1)
+        contador -= 1
+    return invertida
+
+print(palabraInversa("buenas"))
+"""""""""
+
+#llenar array
+
+"""""""""
+def llenarLista():
+    lista = []
+    for i in range(5):
+        print('ingres un numero')
+        lista.append(int(input()))
+    print(lista);
 
 
-x=5;
-y=5;
-
-print(suma(x,y));
-print("\n");
-sumaAcincuenta();
-"""
+llenarLista()
+"""""
